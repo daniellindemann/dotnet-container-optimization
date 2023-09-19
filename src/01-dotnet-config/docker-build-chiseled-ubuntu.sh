4f946c7513ada485a64d0e7bbb6fad405b9e0de0
Daniel Lindemann <d.lindemann@abtis.de>
2023-09-19 17:49:51 +0000

    add docker image build scripts

---
+++ src/01-dotnet-config/docker-build-chiseled-ubuntu.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+SCRIPT_DIR=$(dirname "$0")
+docker build --no-cache -t 01-dotnet-config-chiseled-ubuntu:1.0.0 -t 01-dotnet-config-chiseled-ubuntu:latest -f $SCRIPT_DIR/Dockerfile.chiseled-ubuntu $SCRIPT_DIR
