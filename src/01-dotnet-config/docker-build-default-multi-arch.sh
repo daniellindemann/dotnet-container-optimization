baf36a49e945731a2080ad1a4e030c93d64f00b4
Daniel Lindemann <d.lindemann@abtis.de>
2023-09-22 14:40:02 +0000

    add multi-arch demo scripts

---
+++ src/01-dotnet-config/docker-build-default-multi-arch.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+# to push to azure container registry do the following:
+#  - login to azure container registry: az acr login --name <acrName>
+#  - build the image with acr tag with the following command:
+#       docker buildx build --push --platform linux/amd64,linux/arm64 -t <acrLoginServer>/01-dotnet-config:1.0.0 -f $SCRIPT_DIR/Dockerfile.default-multi-arch $SCRIPT_DIR
+
+
+SCRIPT_DIR=$(dirname "$0")
+docker buildx build $@ \
+    --platform linux/amd64,linux/arm64 \
+    -t 01-dotnet-config:1.0.0 -t 01-dotnet-config:latest -f $SCRIPT_DIR/Dockerfile.default-multi-arch $SCRIPT_DIR
