872fcf6455d5276084c35571a688ca2a3c387472
Daniel Lindemann <d.lindemann@abtis.de>
2023-09-22 15:12:45 +0000

    add info about BUILDKIT_NO_CLIENT_TOKEN

--- src/01-dotnet-config/docker-build-default-multi-arch.sh
+++ src/01-dotnet-config/docker-build-default-multi-arch.sh
@@ -3,7 +3,7 @@
 # to push to azure container registry do the following:
 #  - login to azure container registry: az acr login --name <acrName>
 #  - build the image with acr tag with the following command:
-#       docker buildx build --push --platform linux/amd64,linux/arm64 -t <acrLoginServer>/01-dotnet-config:1.0.0 -f $SCRIPT_DIR/Dockerfile.default-multi-arch $SCRIPT_DIR
+#       BUILDKIT_NO_CLIENT_TOKEN=true docker buildx build --push --platform linux/amd64,linux/arm64 -t <acrLoginServer>/01-dotnet-config:1.0.0 -f $SCRIPT_DIR/Dockerfile.default-multi-arch $SCRIPT_DIR
 
 
 SCRIPT_DIR=$(dirname "$0")
