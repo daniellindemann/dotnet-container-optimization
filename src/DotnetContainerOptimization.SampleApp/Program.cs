a2f2e9a11321104e8aff5bbb1d932966464a79ec
Daniel Lindemann <d.lindemann@abtis.de>
2024-01-28 15:07:54 +0000

    minor fixes

--- src/DotnetContainerOptimization.SampleApp/Program.cs
+++ src/DotnetContainerOptimization.SampleApp/Program.cs
@@ -33,7 +33,7 @@ builder.Services.AddOptions<GreetingsOptions>()
     .ValidateDataAnnotations()
     .ValidateOnStart();
 
-// https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks?view=aspnetcore-7.0
+// https://learn.microsoft.com/en-us/aspnet/core/host-and-deploy/health-checks
 builder.Services.AddHealthChecks();
 
 // add other services
