2a88d9b5fbfefe0ce48d684eb4e0b701b874e4d2
Daniel Lindemann <d.lindemann@abtis.de>
2024-01-28 12:40:18 +0000

    update to chiseled ubuntu

--- scripts/docker/docker-build-self-contained.sh
+++ scripts/docker/docker-build-self-contained.sh
@@ -2,4 +2,4 @@
 
 SCRIPT_DIR=$(dirname "$0")
 APP_DIR=$SCRIPT_DIR/../../src/DotnetContainerOptimization.SampleApp
-docker build $@ -t sample-app-self-contained:1.0.0 -t sample-app-self-contained:latest -f $APP_DIR/Dockerfile.self-contained --no-cache $APP_DIR
+docker build $@ -t sample-app-self-contained:1.0.0 -t sample-app-self-contained:latest -f $APP_DIR/Dockerfile.self-contained $APP_DIR
